04c475777d54dfc3f0e8a86188ebd4dcf70d61df
Egor <36207275+HeavenlyRaven@users.noreply.github.com>
2023-01-24 02:16:13 +0300

    Implemented dependencies handling

--- setup.sh
+++ setup.sh
@@ -5,4 +5,8 @@ mkdir heroes
 echo '{"token": "", "admin_id": null, "group_id": null}' > config.json
 # creating queues.json
 echo '{}' > queues.json
+# creating a virtual environment
+python3 -m venv ./rollbot_env
+# installing dependencies
+./rollbot_env/bin/python3 -m pip install -r requirements.txt
 echo "Bot setup finished successfully. Please, configure it via config.json file."
